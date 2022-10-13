b8b420f7c23d3fce3fb99438361d32273a9b5887
Hojin You <youho@sheridancollege.ca>
2022-10-12 21:33:25 -0400

    Layout for score keeper screen

--- app/src/main/java/sheridan/youho/assignment2/MainActivity.kt
+++ app/src/main/java/sheridan/youho/assignment2/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the Home/Up reset_button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
